c1a40ad50dc1e83c9b2e22f3f0637dcd14fded5e
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-10-28 12:32:29 +0700

    1st release

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+
+import plaid from './plaid';
+import summary from './summary';
+import accounts from './accounts';
+import categories from './categories';
+import transactions from './transactions';
+import subscriptions from './subscriptions';
+
+const app = new Hono().basePath('/api');
+
+const routes = app
+  .route('/plaid', plaid)
+  .route('/summary', summary)
+  .route('/accounts', accounts)
+  .route('/categories', categories)
+  .route('/transactions', transactions)
+  .route('/subscriptions', subscriptions);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof routes;
