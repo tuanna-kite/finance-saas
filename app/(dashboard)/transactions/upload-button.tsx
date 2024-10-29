963eac8b7633a22123044e030d0925dc5331d4b7
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-10-29 12:22:39 +0700

    remove subcription

--- app/(dashboard)/transactions/upload-button.tsx
+++ app/(dashboard)/transactions/upload-button.tsx
@@ -1,9 +1,9 @@
-import { Upload } from "lucide-react";
-import { useCSVReader } from "react-papaparse";
+import { Upload } from 'lucide-react';
+import { useCSVReader } from 'react-papaparse';
 
-import { usePaywall } from "@/features/subscriptions/hooks/use-paywall";
+import { usePaywall } from '@/features/subscriptions/hooks/use-paywall';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 type Props = {
   onUpload: (results: any) => void;
@@ -11,30 +11,26 @@ type Props = {
 
 export const UploadButton = ({ onUpload }: Props) => {
   const { CSVReader } = useCSVReader();
-  const { shouldBlock, triggerPaywall } = usePaywall();
+  // const { shouldBlock, triggerPaywall } = usePaywall();
 
-  if (shouldBlock) {
-    return (
-      <Button
-        size="sm"
-        className="w-full lg:w-auto"
-        onClick={triggerPaywall}
-      >
-        <Upload className="size-4 mr-2" />
-        Import
-      </Button>
-    );
-  }
+  // if (shouldBlock) {
+  //   return (
+  //     <Button
+  //       size="sm"
+  //       className="w-full lg:w-auto"
+  //       onClick={triggerPaywall}
+  //     >
+  //       <Upload className="size-4 mr-2" />
+  //       Import
+  //     </Button>
+  //   );
+  // }
 
   return (
     <CSVReader onUploadAccepted={onUpload}>
       {({ getRootProps }: any) => (
-        <Button
-          size="sm"
-          className="w-full lg:w-auto"
-          {...getRootProps()}
-        >
-          <Upload className="size-4 mr-2" />
+        <Button size='sm' className='w-full lg:w-auto' {...getRootProps()}>
+          <Upload className='size-4 mr-2' />
           Import
         </Button>
       )}
